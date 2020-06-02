1817f69db5594642983f96a6f0f5ea6cf2ab5850
willieLjohnson <liwa.johnson@gmail.com>
2020-06-02 14:33:06 -0400

    Create simple state system

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,13 +4,35 @@ const ACCELERATION = 500
 const MAX_SPEED = 80
 const FRICTION = 500
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
 var velocity = Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
+func _ready() -> void:
+	animationTree.active = true
+	
 func _physics_process(delta: float) -> void:
+	match state:
+		MOVE:
+			move_state(delta)
+		
+		ROLL:
+			pass
+			
+		ATTACK:
+			attack_state(delta)
+	
+
+func move_state(delta: float) -> void:
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -26,3 +48,6 @@ func _physics_process(delta: float) -> void:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	velocity = move_and_slide(velocity) 
+
+func attack_state(delta: float):
+	pass
