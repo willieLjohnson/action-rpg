cbe2538976a7d0c2f8d03df8d1993ceeabd8ec7b
willieLjohnson <liwa.johnson@gmail.com>
2020-06-03 08:21:33 -0400

    Add hurtbox to grasses

--- World/Grass.gd
+++ World/Grass.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("attack"):
-		var grassEffectScene = load("res://Effects/GrassEffect.tscn")
-		var grassEffect = grassEffectScene.instance()
-		var world = get_tree().current_scene
-		world.add_child(grassEffect)
-		grassEffect.global_position = global_position
-		queue_free()
+func create_grass_effect() -> void:
+	var grassEffectScene = load("res://Effects/GrassEffect.tscn")
+	var grassEffect = grassEffectScene.instance()
+	var world = get_tree().current_scene
+	world.add_child(grassEffect)
+	grassEffect.global_position = global_position
+
+
+
+func _on_HurtBox_area_entered(area: Area2D) -> void:
+	create_grass_effect()
+	queue_free()
