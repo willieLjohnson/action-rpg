fba7c1bd9832916a62254a3512328742d2773baf
willieLjohnson <liwa.johnson@gmail.com>
2020-06-06 09:41:04 -0400

    Turn bat in velocity directio when chasing

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -17,6 +17,7 @@ var knockback = Vector2.ZERO
 
 var state = CHASE
 
+onready var sprite = $AnimatedSprite
 onready var stats = $Stats
 onready var playerDetectionZone = $PlayerDetectionZone
 
@@ -37,7 +38,9 @@ func _physics_process(delta: float) -> void:
 			if player != null:
 				var direction = (player.global_position - global_position).normalized()
 				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
-	
+			else:
+				state = IDLE
+			sprite.flip_h = velocity.x < 0
 	velocity = move_and_slide(velocity)
 	
 func seek_player() -> void:
