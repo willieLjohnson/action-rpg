2bb35fb673246d50450fe393077169f29d20f368
willieLjohnson <liwa.johnson@gmail.com>
2020-06-07 14:13:14 -0400

    Refactor bat wander code

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -36,43 +36,42 @@ func _physics_process(delta: float) -> void:
 		IDLE:
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 			seek_player()
-			
 			if wanderController.get_time_left() == 0:
-				state = pick_random_state([IDLE, WANDER])
-				wanderController.start_wander_timer(rand_range(1, 3))
+				update_wander()	
 				
 		WANDER:
 			seek_player()
 			if wanderController.get_time_left() == 0:
-				state = pick_random_state([IDLE, WANDER])
-				wanderController.start_wander_timer(rand_range(1, 3))		
-				
-			var direction = global_position.direction_to(wanderController.target_position)
-			velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
-			sprite.flip_h = velocity.x < 0
-			
+				update_wander()		
+			accelerate_towards_point(wanderController.target_position, delta)
 			if global_position.distance_to(wanderController.target_position) <= WANDER_TARGET_RANGE:
-				state = pick_random_state([IDLE, WANDER])
-				wanderController.start_wander_timer(rand_range(1, 3))
+				update_wander()
 				
 		CHASE: 
 			var player = playerDetectionZone.player
 			if player:
-				var direction = global_position.direction_to(player.global_position)
-				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+				accelerate_towards_point(player.global_position, delta)
 			else:
 				state = IDLE
-			sprite.flip_h = velocity.x < 0
 			
 	if softCollision.is_colliding():
 		velocity += softCollision.get_push_vector() * delta * 400
 		
 	velocity = move_and_slide(velocity)
-	
+
+func accelerate_towards_point(point: Vector2, delta: float) -> void:
+	var direction = global_position.direction_to(point)
+	velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+	sprite.flip_h = velocity.x < 0
+			
 func seek_player() -> void:
 	if playerDetectionZone.can_see_player():
 		state = CHASE
 
+func update_wander():
+	state = pick_random_state([IDLE, WANDER])
+	wanderController.start_wander_timer(rand_range(1, 3))
+				
 func pick_random_state(state_list: Array):
 	state_list.shuffle()
 	return state_list.pop_front()
