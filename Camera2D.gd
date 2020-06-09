b210dbc6f7a363a732addd4ef797d3d21e3d446b
willieLjohnson <liwa.johnson@gmail.com>
2020-06-09 15:30:51 -0400

    Update game a lil bitty (polish)

--- Camera2D.gd
+++ Camera2D.gd
@@ -1,10 +1,12 @@
 extends Camera2D
 
+export(bool) var useLimits = true
 onready var topLeft = $Limits/TopLeft
 onready var bottomRight = $Limits/BottomRight
 
 func _ready() -> void:
-	limit_top = topLeft.position.y
-	limit_left = topLeft.position.x
-	limit_bottom = bottomRight.position.y
-	limit_right = bottomRight.position.x
+		if useLimits:
+			limit_top = topLeft.position.y
+			limit_left = topLeft.position.x
+			limit_bottom = bottomRight.position.y
+			limit_right = bottomRight.position.x
